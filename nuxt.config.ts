22509e28ab6cae1ba06f2580a9833bfec67a6e5f
unva <101005848+unwanation@users.noreply.github.com>
2024-03-04 22:38:33 +0700

    build fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
+import topLevelAwait from 'vite-plugin-top-level-await';
+
 export default defineNuxtConfig({
-				components: [
-								{
-												path: '~/components',
-												pathPrefix: false,
-								},
-				],
-				ssr: false,
-				css: ['~/assets/vars.css', '~/assets/fonts.css', '~/assets/main.css'],
-				devtools: { enabled: true },
-				modules: ['nuxt-icon', '@nuxt/image', 'vue3-carousel-nuxt'],
+	components: [
+		{
+			path: '~/components',
+			pathPrefix: false,
+		},
+	],
+	ssr: false,
+	css: ['~/assets/vars.css', '~/assets/fonts.css', '~/assets/main.css'],
+	devtools: { enabled: true },
+	modules: ['nuxt-icon', '@nuxt/image', 'vue3-carousel-nuxt'],
+	vite: {
+		plugins: [
+			topLevelAwait({
+				promiseExportName: '__tla',
+				promiseImportName: i => `__tla_${i}`,
+			}),
+		],
+	},
 });
