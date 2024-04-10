75dfcb413d514158729a7aa92586bbea2ba17e50
unva <101005848+unwanation@users.noreply.github.com>
2024-04-10 18:28:38 +0700

    sidebar fix

--- components/ui/SidebarElement.vue
+++ components/ui/SidebarElement.vue
@@ -6,30 +6,37 @@ defineProps<{
 </script>
 
 <template>
-	<div
+	<button
 		@click="store.setCurrentTab(tab)"
+		:disabled="store.currentTab() == tab"
 		:class="{
 			'sidebar-element': true,
 			'sidebar-element_active': tab == store.currentTab(),
 		}"
 	>
 		{{ label }}
-	</div>
+	</button>
 </template>
 
 <style lang="scss">
 .sidebar-element {
 	display: block;
+	width: 100%;
 	padding: 0.5rem;
 	margin: 0.5rem 0;
 	border-radius: 12px;
 	font-weight: 500;
+	text-align: left;
+	background: none;
+	border: none;
+	color: var(--text);
 	transition: all 0.5s;
 
 	&:active,
 	&:focus,
 	&:hover {
 		color: var(--accent);
+		cursor: pointer;
 		outline: none;
 	}
 
@@ -37,6 +44,19 @@ defineProps<{
 		background-color: var(--accent);
 		&:hover {
 			color: var(--text);
+		}
+	}
+}
+
+@media (prefers-color-scheme: light) {
+	.sidebar-element {
+		&_active {
+			color: var(--bg);
+			&:active,
+			&:focus,
+			&:hover {
+				color: var(--bg);
+			}
 		}
 	}
 }
