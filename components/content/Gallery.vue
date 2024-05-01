270c2bbb1fb4d8ac29b430ec141b9ae709d5fd60
unva <101005848+unwanation@users.noreply.github.com>
2024-05-01 23:10:55 +0700

    gallery QoL improvement

--- components/content/Gallery.vue
+++ components/content/Gallery.vue
@@ -1,22 +1,30 @@
 <script setup>
 const currentSlide = ref(1);
+
+const moveToPreviousSlide = () => {
+	if (currentSlide.value == 1) {
+		currentSlide.value = 26;
+		return;
+	}
+	currentSlide.value--;
+};
+
+const moveToNextSlide = () => {
+	if (currentSlide.value == 26) {
+		currentSlide.value = 1;
+		return;
+	}
+	currentSlide.value++;
+};
 </script>
 
 <template>
 	<div class="gallery">
-		<button
-			v-show="currentSlide > 1"
-			class="button"
-			@click="currentSlide--"
-		>
+		<button class="button" @click="moveToPreviousSlide">
 			<Icon name="material-symbols:arrow-back-rounded" />
 		</button>
 		<img :src="`/gallery/${currentSlide}.jpg`" alt="" class="photo" />
-		<button
-			v-show="currentSlide < 26"
-			class="button"
-			@click="currentSlide++"
-		>
+		<button class="button" @click="moveToNextSlide">
 			<Icon name="material-symbols:arrow-forward-rounded" />
 		</button>
 	</div>
